b3a77a725b3d7c9a162e6f0f36a439eaa8e65d1e
Poonam <poonam@gmail.com>
2026-04-22 16:49:46 +0530

    databse connection fixed

--- backend_inventory/db.js
+++ backend_inventory/db.js
@@ -1,10 +1,18 @@
 const mysql = require("mysql2");
 
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: "usersh",
+//   password: "",
+//   database: "inventory"
+// });
+
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: "usersh",
-  password: "",
-  database: "inventory"
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT   // 👈 
 });
 
 db.connect((err) => {
