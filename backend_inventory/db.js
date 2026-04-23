f6143782754cc0b1a53cf6be872d2b576116efb6
Poonam <poonam@gmail.com>
2026-04-23 15:48:08 +0530

    show sql error

--- backend_inventory/db.js
+++ backend_inventory/db.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  database: process.env.MYSQLDATABASE,
   port: process.env.DB_PORT   // 👈 
 });
 
