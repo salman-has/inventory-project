cadff2e0fb261a780ed0e03c188a910659e42a04
Poonam <poonam@gmail.com>
2026-04-23 16:10:40 +0530

    show sql error

--- backend_inventory/db.js
+++ backend_inventory/db.js
@@ -13,6 +13,7 @@ const db = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
+  password:process.env.MYSQL_ROOT_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQLPORT,
   waitForConnections: true,
