388f1b02b26c8f065829212bb1db919eda2bd2f5
Poonam <poonam@gmail.com>
2026-04-23 16:06:12 +0530

    show sql error

--- backend_inventory/db.js
+++ backend_inventory/db.js
@@ -9,12 +9,15 @@ const mysql = require("mysql2");
 // });
 
 //hosted databsser Railway...
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+const db = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: process.env.DB_PORT   // 👈 
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 db.connect((err) => {
